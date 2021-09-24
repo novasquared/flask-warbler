549bd4c8e8168c4f5c51c1f75d9207b42e0d4289
davisrao <davisrao1@gmail.com>
2021-09-24 15:03:39 -0700

    updated test_user_models to test login and authentication

--- test_user_model.py
+++ test_user_model.py
@@ -10,6 +10,7 @@ from unittest import TestCase
 
 from models import db, User, Message, Follows, Likes
 from flask_bcrypt import Bcrypt
+from sqlalchemy import exc
 
 
 # BEFORE we import our app, let's set an environmental variable
@@ -47,15 +48,15 @@ class UserModelTestCase(TestCase):
 
         test_u1 = User(email="testemail1@test.com",
                         username="testuser1",
-                        password="hashedpass1")
+                        password="pass1")
 
         test_u2 = User(email="testemail2@test.com",
                         username="testuser2",
-                        password="hashedpass2")
+                        password="pass2")
 
         test_u3 = User(email="testemail3@test.com",
                         username="testuser3",
-                        password="hashedpass3")
+                        password="pass3")
 
 
         db.session.add(test_u1)
@@ -101,13 +102,54 @@ class UserModelTestCase(TestCase):
         """Does signup return the correct user"""
 
         test_user_sign_up = User.signup("Jeanne", "test@test.com", "testing123", None)
+        test_user_sign_up_2 = User.signup("Davis", "test1@test.com", "testing123", "/static/images/pic.png")
+
 
         db.session.commit()
 
+        #tests successful username entered
         self.assertEqual(test_user_sign_up.username, "Jeanne")
+
+        #tests successful email entered
         self.assertEqual(test_user_sign_up.email, "test@test.com")
 
+        #tests default image link on NONE entered
+        self.assertEqual(test_user_sign_up.image_url, "/static/images/default-pic.png")
+
+        #tests successful image link entered
+        self.assertEqual(test_user_sign_up_2.image_url, "/static/images/pic.png")
+
+
+        #tests successful header image entered in DB
+        self.assertEqual(test_user_sign_up.header_image_url, "/static/images/warbler-hero.jpg")
+
+
+        #tests successful password hash
         self.assertTrue(bcrypt.check_password_hash(test_user_sign_up.password, "testing123"))
+
+    def test_unsuccessful_email_signup(self):
+        """testing if error raises on blank email"""
+        test_user_sign_up = User.signup("Jeanne", None, "testing123", None)
+
+        db.session.add(test_user_sign_up)
+
+        with self.assertRaises(exc.IntegrityError):
+            db.session.commit()
+
+    def test_unsuccessful_username_signup(self):
+        """testing if error raises on blank UN"""
+        test_user_sign_up = User.signup(None, "test123@test.com", "testing123", None)
+
+        db.session.add(test_user_sign_up)
+
+        with self.assertRaises(exc.IntegrityError):
+            db.session.commit()
+    
+    def test_unsuccessful_password_signup(self):
+        """testing if error raises on blank password"""
+
+        with self.assertRaises(ValueError):
+            User.signup("Jeanne", "test1234@test.com", None , None)
 
     def test_successful_follow(self):
         """does is following detect succeesful follow
@@ -131,7 +173,34 @@ class UserModelTestCase(TestCase):
         self.assertFalse(user_1.is_followed_by(user_2))
 
 
+    def test_successful_authentication(self):
+        """checks to see if user is successfully authenticated with correct UN and PW"""
 
-    
-    #does user.signup detect correct user, and does it error on incorrect credentials
-    #does user.authenticate find correct user or fail to authenticate when user not found based on email / password
+
+        user_4 = User.signup(
+                username="my_user",
+                password="test_my_user",
+                email="email@testuser.com",
+                image_url=User.image_url.default.arg,
+            )
+
+        db.session.commit()
+
+        self.assertTrue(user_4.authenticate(user_4.username,"test_my_user"))
+
+    def test_failed_password_authentication(self):
+        """checks to see if user is NOT authenticated with incorrect PW"""
+        user_1 = User.query.get(self.test_u1_id)
+
+        with self.assertRaises(ValueError):
+            user_1.authenticate(user_1.username,"12345")
+
+    def test_failed_username_authentication(self):
+        """checks to see if user is NOT authenticated with incorrect UN"""
+        user_1 = User.query.get(self.test_u1_id)
+
+        self.assertFalse(user_1.authenticate("123","pass1"))
+
+
+
+    #does user.authenticate find correct user or fail to authenticate when user not found based on UN / password
